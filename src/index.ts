9b20e9603b32742cfe5828324e84b595c37697a8
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-03-06 17:43:25 +0100

    try to compose

--- src/index.ts
+++ src/index.ts
@@ -11,15 +11,11 @@ import { IPathPosition } from '@stoplight/types/parsers';
 import { IOEither, tryCatch2v } from 'fp-ts/lib/IOEither';
 import { fromNullable } from 'fp-ts/lib/Option';
 import { IO } from 'fp-ts/lib/IO';
-import { Task } from 'fp-ts/lib/Task';
 import { fromIOEither, tryCatch } from 'fp-ts/lib/TaskEither';
 import { Either } from 'fp-ts/lib/Either';
 import * as t from 'io-ts'
 import { error, log } from 'console';
 import { failure } from 'fp-ts/lib/Validation';
-
-const map2 = <A, B, C>(fa: IOEither<unknown, A>, fb: IOEither<unknown, B>, f: (a: A, b: B) => C): IOEither<unknown, C> =>
-  fa.chain(a => fb.map(b => f(a, b)))
 
 const Config = t.strict({
   GITHUB_EVENT_PATH: t.string,
@@ -67,8 +63,8 @@ createConfigFromEnv
     SPECTRAL_FILE_PATH
   }) => {
 
-    const repository =
-      tryCatch2v<NodeJS.ErrnoException, Event>(() => require(GITHUB_EVENT_PATH), e => e as NodeJS.ErrnoException)
+    const getRepositoryInfoFromEvent =
+      fromIOEither(tryCatch2v<string, Event>(() => require(GITHUB_EVENT_PATH), e => String(e)))
         .map(event => {
           const { repository } = event
           const { owner: { login: owner } } = repository
@@ -76,64 +72,71 @@ createConfigFromEnv
           return { owner, repo };
         });
 
-    const octokit = tryCatch2v(() => new Octokit({ auth: `token ${GITHUB_TOKEN}` }), r => String(r));
+    const createOctokitInstance =
+      fromIOEither(tryCatch2v(() => new Octokit({ auth: `token ${GITHUB_TOKEN}` }), e => String(e)));
 
-    return map2(repository, octokit, ({ owner, repo }, kit) => {
-      return tryCatch(() => kit.checks.create({ owner, repo, name: 'Spectral Lint Check', head_sha: GITHUB_SHA }), e => String(e))
-        .chain(check =>
-          fromIOEither(tryCatch2v(() => readFileSync(join(GITHUB_WORKSPACE, SPECTRAL_FILE_PATH), { encoding: 'utf8' }), e => String(e))
-            .chain(fileContent => tryCatch2v(() => parseWithPointers(fileContent), e => String(e)))
-            .chain(parsed => tryCatch2v(() => ({ parsed, results: createSpectral().run(parsed.data, { resolvedTarget: parsed.data }).results }), e => String(e)))
-            .map(({ results, parsed }) => {
-              const defaultAnnotation: Required<IPathPosition> = {
-                start: { line: 0, column: undefined },
-                end: { line: 0, column: undefined }
-              };
+    const readFileToAnalyze =
+      tryCatch2v(() => readFileSync(join(GITHUB_WORKSPACE, SPECTRAL_FILE_PATH), { encoding: 'utf8' }), e => String(e))
 
-              const annotations: Octokit.ChecksUpdateParamsOutputAnnotations[] = results.map(validationResult => {
-                const path = pathToPointer(validationResult.path as string[]).slice(1);
-                const position = fromNullable(parsed.pointers[path])
-                  .filter(isDefined)
-                  .map(pointer => ({ ...defaultAnnotation, ...pointer }))
-                  .getOrElse(defaultAnnotation);
+    const parseJsonWithPointers =
+      (fileContent: string) => tryCatch2v(() => parseWithPointers(fileContent), e => String(e))
 
-                const annotation_level: "notice" | "warning" | "failure"
-                  = validationResult.severity === ValidationSeverity.Error ? 'failure'
-                    : validationResult.severity === ValidationSeverity.Warn ? 'warning' : 'notice';
+    return getRepositoryInfoFromEvent.chain(event => createOctokitInstance.map(octokit => ({ event, octokit })))
+      .chain(({ event, octokit }) => {
+        return tryCatch(() => octokit.checks.create({ owner: event.owner, repo: event.repo, name: 'Spectral Lint Check', head_sha: GITHUB_SHA }), e => String(e))
+          .chain(check =>
+            fromIOEither(readFileToAnalyze
+              .chain(parseJsonWithPointers)
+              .chain(parsed => tryCatch2v(() => ({ parsed, results: createSpectral().run(parsed.data, { resolvedTarget: parsed.data }).results }), e => String(e))))
+              .chain(({ results, parsed }) => {
+                const defaultAnnotation: Required<IPathPosition> = {
+                  start: { line: 0, column: undefined },
+                  end: { line: 0, column: undefined }
+                };
 
-                return {
-                  annotation_level,
-                  message: validationResult.summary,
-                  title: validationResult.name,
-                  start_line: position.start.line,
-                  end_line: position.end.line,
-                  start_column: position.start.column,
-                  end_column: position.end.column,
-                  path: SPECTRAL_FILE_PATH
-                }
+                const annotations: Octokit.ChecksUpdateParamsOutputAnnotations[] = results.map(validationResult => {
+                  const path = pathToPointer(validationResult.path as string[]).slice(1);
+                  const position = fromNullable(parsed.pointers[path])
+                    .filter(isDefined)
+                    .map(pointer => ({ ...defaultAnnotation, ...pointer }))
+                    .getOrElse(defaultAnnotation);
+
+                  const annotation_level: "notice" | "warning" | "failure"
+                    = validationResult.severity === ValidationSeverity.Error ? 'failure'
+                      : validationResult.severity === ValidationSeverity.Warn ? 'warning' : 'notice';
+
+                  return {
+                    annotation_level,
+                    message: validationResult.summary,
+                    title: validationResult.name,
+                    start_line: position.start.line,
+                    end_line: position.end.line,
+                    start_column: position.start.column,
+                    end_column: position.end.column,
+                    path: SPECTRAL_FILE_PATH
+                  }
+                });
+
+                return tryCatch(() => octokit.checks.update({
+                  check_run_id: check.data.id,
+                  owner: event.owner,
+                  name: 'Spectral Lint Check',
+                  repo: event.repo,
+                  status: 'completed',
+                  conclusion: 'failure',
+                  completed_at: (new Date()).toISOString(),
+                  output: {
+                    title: 'Spectral Lint Check',
+                    summary: 'This was horrible',
+                    annotations
+                  }
+                }), e => String(e))
               })
-
-              return new Task(() => kit.checks.update({
-                check_run_id: check.data.id,
-                owner,
-                name: 'Spectral Lint Check',
-                repo,
-                status: 'completed',
-                conclusion: 'failure',
-                completed_at: (new Date()).toISOString(),
-                output: {
-                  title: 'Spectral Lint Check',
-                  summary: 'This was horrible',
-                  annotations
-                }
-              }))
-            })
           )
-        )
-    })
-  }).run()
+      })
+  })
+  .run()
   .then(result => result.fold(
     err => error(failure(err)),
     () => log('Worked fine')
-  )
-  ).catch(error);
+  ));
