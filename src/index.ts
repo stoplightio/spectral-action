f6a3acc9ab8661de0e1866b741e50dff79564885
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-11-13 10:33:08 -0600

    create inner loop

--- src/index.ts
+++ src/index.ts
@@ -146,12 +146,12 @@ const program = pipe(
     )
   ),
   TE.map(({ checkResponse, repositoryInfo, annotations }) => {
-    info(
-      `Check run '${checkResponse.data.name}' concluded with '${checkResponse.data.conclusion}' (${checkResponse.data.html_url})`
-    );
-    info(
-      `Commit ${repositoryInfo.sha} has been annotated (https://github.com/${repositoryInfo.owner}/${repositoryInfo.repo}/commit/${repositoryInfo.sha})`
-    );
+    checkResponse.map(res => {
+      info(`Check run '${res.data.name}' concluded with '${res.data.conclusion}' (${res.data.html_url})`);
+      info(
+        `Commit ${repositoryInfo.sha} has been annotated (https://github.com/${repositoryInfo.owner}/${repositoryInfo.repo}/commit/${repositoryInfo.sha})`
+      );
+    });
 
     const fatalErrors = annotations.filter(a => a.annotation_level === 'failure');
     if (fatalErrors.length > 0) {
