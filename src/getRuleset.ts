67965be9391f16c4700551f57b4ba9da4a4815fb
Jakub Rożek <jakub@stoplight.io>
2022-05-27 15:45:37 +0200

    feat(core): use spectral-ruleset-bundler

--- src/getRuleset.ts
+++ src/getRuleset.ts
@@ -1,13 +1,13 @@
-import { Optional } from '@stoplight/types';
-import { Ruleset, RulesetDefinition } from '@stoplight/spectral-core';
 import * as fs from 'fs';
-import * as path from 'path';
 import * as process from 'process';
-import { migrateRuleset } from '@stoplight/spectral-ruleset-migrator';
+import type { Optional } from '@stoplight/types';
+import { Ruleset } from '@stoplight/spectral-core';
 import { info, error } from '@actions/core';
-
-// eslint-disable-next-line @typescript-eslint/require-await
-const AsyncFunction = (async (): Promise<void> => void 0).constructor as FunctionConstructor;
+import * as path from '@stoplight/path';
+import type { IO } from '@stoplight/spectral-ruleset-bundler';
+import { builtins } from '@stoplight/spectral-ruleset-bundler/plugins/builtins';
+import { commonjs } from '@stoplight/spectral-ruleset-bundler/plugins/commonjs';
+import { bundleAndLoadRuleset } from '@stoplight/spectral-ruleset-bundler/with-loader';
 
 async function getDefaultRulesetFile(): Promise<Optional<string>> {
   const cwd = process.cwd();
@@ -29,43 +29,18 @@ export async function getRuleset(rulesetFile: Optional<string>): Promise<Ruleset
 
   if (!rulesetFile) {
     throw new Error(
-      'No ruleset has been found. Please provide a ruleset using the --ruleset CLI argument, or make sure your ruleset file matches .?spectral.(js|ya?ml|json)'
+      'No ruleset has been found. Please provide a ruleset using the spectral_ruleset option, or make sure your ruleset file matches .?spectral.(js|ya?ml|json)'
     );
   }
 
   info(`Loading ruleset '${rulesetFile}'...`);
 
-  let ruleset;
+  const io: IO = { fetch, fs };
 
   try {
-    if (/(json|ya?ml)$/.test(path.extname(rulesetFile))) {
-      const m: { exports?: RulesetDefinition } = {};
-      const paths = [path.dirname(rulesetFile), __dirname];
-
-      await AsyncFunction(
-        'module, require',
-        await migrateRuleset(rulesetFile, {
-          format: 'commonjs',
-          fs,
-        })
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-      )(m, (id: string) => require(require.resolve(id, { paths })) as unknown);
-
-      ruleset = m.exports;
-    } else {
-      const imported = (await import(rulesetFile)) as { default: unknown } | unknown;
-      ruleset =
-        typeof imported === 'object' && imported !== null && 'default' in imported
-          ? (imported as Record<'default', unknown>).default
-          : imported;
-    }
+    return await bundleAndLoadRuleset(rulesetFile, io, [commonjs(), builtins()]);
   } catch (e) {
-    error(`Failed to load ruleset '${rulesetFile}'... Error: ${e.message}`);
+    error(`Failed to load ruleset '${rulesetFile}'... Error: ${String(e)}`);
     throw e;
   }
-
-  return new Ruleset(ruleset, {
-    severity: 'recommended',
-    source: rulesetFile,
-  });
 }
